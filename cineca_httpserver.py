390349dc7138c10f3ef01123c1d0c51953bdaae6
Pierre-André <pierre-andre.michel@isb-sib.ch>
2021-07-27 17:11:01 +0200

    added config files to customize access to remote URLs accorrding to PROD / DEV environments

--- cineca_httpserver.py
+++ cineca_httpserver.py
@@ -2,9 +2,12 @@ import os
 import sys
 import json
 import tarfile
+import argparse
 from ftplib import FTP
 import http.client
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import datetime
+
 
 class GP(BaseHTTPRequestHandler):
     def _set_headers(self,statusCode, content_type):
@@ -46,20 +49,36 @@ class GP(BaseHTTPRequestHandler):
         self._set_headers(statusCode, 'application/xml; charset=utf-8')
         self.wfile.write(bytes(somexml,'utf-8'))
 
+    def get_remote_connection(self, service):
+        protocol = properties[service + '.protocol']
+        server = properties[service + '.server']
+        if (protocol == 'https'):
+            return http.client.HTTPSConnection(server)
+        else:
+            return http.client.HTTPConnection(server)
+
+    def get_remote_baseurl(self, service):
+        return properties[service + '.baseurl']
+
     def do_GET(self):
 
         error_msg='ERROR, invalid URL: ' + self.path
 
         try:
 
-            print('GET request', self.path, flush=True)
+            log_it('GET request', self.path)
+            log_it('env: ' + env)
 
             if self.path[0:32]=='/bitem/cineca/proxy/ega_studies/':
+                # - - - During DEV it was useful
+                #connection = http.client.HTTPSConnection("denver.text-analytics.ch")
+                #url = '/bitem/ega_studies/' + self.path[32:]
+                # - - - When this service is on PROD on denver server
+                # connection = http.client.HTTPConnection("localhost:9200") # localhost = denver
+                connection = self.get_remote_connection('EGA_STUDIES')
+                url = self.get_remote_baseurl('EGA_STUDIES') + self.path[32:]
 
-                #connection = http.client.HTTPConnection("localhost:9200")
-                connection = http.client.HTTPSConnection("denver.text-analytics.ch")
-                url = '/bitem/ega_studies/' + self.path[32:]
-                print('PROXY', url, flush=True)
+                log_it('PROXY', url)
                 connection.request("GET", url)
                 response = connection.getresponse()
                 if response.status != 200:
@@ -67,7 +86,7 @@ class GP(BaseHTTPRequestHandler):
                     obj = self.buildErrorResponseObject(self.path, error_msg)
                     self.sendJsonResponse(obj,400)
                     return
-                    
+
                 data = response.read().decode("utf-8")
                 obj = json.loads(data)
                 studies = list()
@@ -83,7 +102,7 @@ class GP(BaseHTTPRequestHandler):
             elif self.path[0:25]=='/bitem/cineca/proxy/fake/':
                 fname = self.path[25:]
                 fullname = base_dir + fname
-                print('serving fake file: ' + fullname, flush=True)
+                log_it('serving fake file', fullname)
 
                 data=None
                 nf = open(fullname, 'r')
@@ -107,23 +126,56 @@ class GP(BaseHTTPRequestHandler):
         except:
             error_msg = str(sys.exc_info()[1])
 
-        print(error_msg, flush=True)
+        log_it(error_msg)
         obj = self.buildErrorResponseObject(self.path, error_msg)
         self.sendJsonResponse(obj,400)
 
 
-def run(server_class=HTTPServer, handler_class=GP, port=8088):
-    server_address = ('', port)
-    httpd = server_class(server_address, handler_class)
-    print('Cineca Server running at localhost:8088...', flush=True)
+def log_it(*things):
+    now = datetime.datetime.now().isoformat().replace('T',' ')[:19]
+    print(now, *things, flush=True)
+
+def get_properties():
+    props = dict()
+    prop_file='cineca_httpserver.config.' + env
+    f_in = open(prop_file, 'r')
+    while True:
+        line = f_in.readline()
+        if line == '': break
+        if line[0:1] == '#' : continue
+        if '=' in line:
+            nv = line.split('=')
+            name = nv[0].strip()
+            value = nv[1].strip()
+            props[name]=value
+    f_in.close()
+    return props
+
+def run(host, port, env):
+    server_address = (host, port)
+    httpd = HTTPServer(server_address, GP)
+    log_it('Cineca Server ' + env + ' running at ' + host + ':' + str(port) + '...')
     httpd.serve_forever()
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Main
 # - - - - - - - - - - - - - - - - - - - - - - - - - -
-base_dir="./data/"
 
 if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description="Run a simple HTTP proxy for cineca services")
+    parser.add_argument("-s", "--server", default="localhost", help="IP address on which the server listens")
+    parser.add_argument("-p", "--port", type=int, default=8088, help="port on which the server listens")
+    parser.add_argument("-e", "--env", default="PROD", help="Environment of the server: DEV or PROD")
+    args = parser.parse_args()
+
+    env = args.env
+    log_it('env', env)
+    base_dir="./data/"
+    log_it('base_dir', base_dir)
+    properties = get_properties()
+    for k in properties: log_it('property', k, '=', properties[k])
+
     sys.stdout = open('cineca_python_proxy.log', 'w')
     sys.stderr = open('cineca_python_proxy.err', 'w')
-    run()
+    run(host=args.server, port=args.port, env=args.env)
